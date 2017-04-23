ad4191995cd9abb011335deaea2e8c38366fb05f
chooblarin <choo.bla.rin@gmail.com>
2017-04-23 21:29:05 +0900

    Hello

--- app/index.js
+++ app/index.js
@@ -1,1 +1,27 @@
-console.log('Hello');
+import {run} from '@cycle/run';
+import {div, label, input, hr, h1, makeDOMDriver} from '@cycle/dom';
+
+function main(sources) {
+  const vdom$ = sources.DOM
+    .select('.myinput').events('input')
+    .map(ev => ev.target.value)
+    .startWith('')
+    .map(name =>
+      div([
+        label('Name:'),
+        input('.myinput', {attrs: {type: 'text'}}),
+        hr(),
+        h1(`Hello ${name}`)
+      ])
+    );
+  const sink = {
+    DOM: vdom$
+  }
+  return sink;
+}
+
+const drivers = {
+  DOM: makeDOMDriver('body')
+}
+
+run(main, drivers)
