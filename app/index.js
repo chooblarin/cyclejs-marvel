cbe0122f045e018d38822c96634cb7aa8b9ebd20
chooblarin <choo.bla.rin@gmail.com>
2017-04-23 23:39:49 +0900

    Hello MVI

--- app/index.js
+++ app/index.js
@@ -1,21 +1,49 @@
+import xs from 'xstream';
 import {run} from '@cycle/run';
-import {div, label, input, hr, h1, makeDOMDriver} from '@cycle/dom';
+import {div, label, input, hr, h2, makeDOMDriver} from '@cycle/dom';
+
+function intent(DOMSource) {
+  const changeWeight$ = DOMSource.select('.weight').events('input')
+    .map(ev => ev.target.value);
+  const changeHeight$ = DOMSource.select('.height').events('input')
+    .map(ev => ev.target.value);
+  return {changeWeight$, changeHeight$}
+}
+
+function model(changeWeight$, changeHeight$) {
+  return xs.combine(
+      changeWeight$.startWith(70),
+      changeHeight$.startWith(170)
+    )
+    .map(([weight, height]) => {
+      const heightMeters = height * 0.01;
+      const bmi = Math.round(weight / (heightMeters * heightMeters));
+      return {weight, height, bmi}
+    });
+}
+
+function view(state$) {
+  return state$.map(state =>
+    div([
+      div([
+        label(`Weight: ${state.weight}kg`),
+        input('.weight', {attrs: {type: 'range', min: 40, max: 150, value: 70}})
+      ]),
+      div([
+        label(`Height: ${state.height}cm`),
+        input('.height', {attrs: {type: 'range', min: 140, max: 220, value: 170}})
+      ]),
+      h2(`BMI is ${state.bmi}`)
+    ])
+  )
+}
 
 function main(sources) {
-  const vdom$ = sources.DOM
-    .select('.myinput').events('input')
-    .map(ev => ev.target.value)
-    .startWith('')
-    .map(name =>
-      div([
-        label('Name:'),
-        input('.myinput', {attrs: {type: 'text'}}),
-        hr(),
-        h1(`Hello ${name}`)
-      ])
-    );
+  const {changeWeight$, changeHeight$} = intent(sources.DOM);
+  const state$ = model(changeWeight$, changeHeight$);
+  const vtree$ = view(state$);
   const sink = {
-    DOM: vdom$
+    DOM: vtree$
   }
   return sink;
 }
